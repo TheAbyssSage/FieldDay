60bd7b3757cba79560d6844ff47940406a27890f
TheAbyssSage <sage.stockmans@pm.me>
2026-06-04 14:51:23 +0200

    Add teacher model with migration and fillable fields

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Teacher extends Model
 {
-    //
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
 }
