99828db4b540a4e7567a755d0cb24f330b069036
TheAbyssSage <sage.stockmans@pm.me>
2026-06-05 12:58:03 +0200

    feat: add TeacherSeeder with faker data

--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -2,16 +2,31 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\Teacher;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class TeacherSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $role = Role::where('name', 'teacher')->first();
+
+        for ($i = 0; $i < 10; $i++) {
+            Teacher::create([
+                'first_name' => fake()->firstName(),
+                'last_name' => fake()->lastName(),
+                'email' => fake()->unique()->safeEmail(),
+                'phone_number' => fake()->phoneNumber(),
+                'password' => bcrypt('password'),
+                'role_id' => $role->id,
+            ]);
+        }
     }
 }
