d6a7d3b9501d4eb49f9b77911732e4977decec4f
SirRicharod <riccardo.contact@proton.me>
2026-06-05 11:41:45 +0200

    fix added missing foreignId 'role_id'

--- database/migrations/2026_06_04_123641_create_guardians_table.php
+++ database/migrations/2026_06_04_123641_create_guardians_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone_number');
             $table->string('password');
+            $table->foreignId('role_id')->constrained();
             $table->timestamps();
         });
     }
