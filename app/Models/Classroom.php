61c9db0f469ad526ec1b404416749d105bb69b94
SirRicharod <riccardo.contact@proton.me>
2026-06-05 12:21:41 +0200

    feat: Relations between Classroom and Teacher models

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Classroom extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'teacher_id',
+    ];
+
+    public function teacher(): BelongsTo
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
+
+    public function fieldTrips()
+    {
+        return $this->hasMany(FieldTrip::class);
+    }
 }
