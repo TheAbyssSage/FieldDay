2c338751c1e3be4915410240bdd09bcce743229b
TheAbyssSage <sage.stockmans@pm.me>
2026-06-05 13:28:52 +0200

    feat: add StudentSeeder with faker data and guardian pivot

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,16 +2,37 @@
 
 namespace Database\Seeders;
 
+use App\Models\Classroom;
+use App\Models\Guardian;
+use App\Models\Student;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class StudentSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $classrooms = Classroom::all();
+        $guardians = Guardian::all();
+
+        foreach ($classrooms as $classroom) {
+            for ($i = 0; $i < fake()->numberBetween(5, 15); $i++) {
+                $student = Student::create([
+                    'first_name' => fake()->firstName(),
+                    'last_name' => fake()->lastName(),
+                    'date_of_birth' => fake()->dateTimeBetween('-14 years', '-5 years')->format('Y-m-d'),
+                    'classroom_id' => $classroom->id,
+                ]);
+
+                $student->guardians()->attach(
+                    $guardians->random(fake()->numberBetween(1, 2))->pluck('id')
+                );
+            }
+        }
     }
 }
