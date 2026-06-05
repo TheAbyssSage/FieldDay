43967c0270b014dd6205db603be055de06a5b5f5
SirRicharod <riccardo.contact@proton.me>
2026-06-05 10:23:24 +0200

    feat: create guardian_student migration

--- database/migrations/2026_06_05_081833_create_guardian_student_table.php
+++ database/migrations/2026_06_05_081833_create_guardian_student_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('guardian_student', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('guardian_id')->constrained()->onDelete('cascade');
+            $table->foreignId('student_id')->constrained()->onDelete('cascade');
+            $table->unique(['guardian_id', 'student_id']);
             $table->timestamps();
         });
     }
