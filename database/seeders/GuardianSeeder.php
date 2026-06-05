b7fb50217f2db95509b85c813eb1bd4e25b0f69b
TheAbyssSage <sage.stockmans@pm.me>
2026-06-05 13:04:07 +0200

    feat: add GuardianSeeder with faker data

--- database/seeders/GuardianSeeder.php
+++ database/seeders/GuardianSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
+use App\Models\Guardian;
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class GuardianSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $role = Role::where('name', 'guardian')->first();
+
+        for ($i = 0; $i < 20; $i++) {
+            Guardian::create([
+                'first_name' => fake()->firstName(),
+                'last_name' => fake()->lastName(),
+                'email' => fake()->unique()->safeEmail(),
+                'phone_number' => fake()->phoneNumber(),
+                'password' => bcrypt('password'),
+                'role_id' => $role->id,
+            ]);
+        }
     }
 }
