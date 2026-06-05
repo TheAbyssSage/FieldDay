55bbd692a941230fba37a3c4c8ad83bab20557c3
SirRicharod <riccardo.contact@proton.me>
2026-06-05 10:34:14 +0200

    feat: create payments table migration

--- database/migrations/2026_06_05_082615_create_payments_table.php
+++ database/migrations/2026_06_05_082615_create_payments_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('guardian_id')->constrained();
+            $table->foreignId('student_id')->constrained();
+            $table->foreignId('field_trip_id')->constrained();
+            $table->decimal('amount', 8, 2);
+            $table->enum('status', ['pending', 'paid', 'refunded'])->default('pending'); // PENDING - PAID - REFUNDED
+            $table->timestamp('paid_at')->nullable();
             $table->timestamps();
         });
     }
