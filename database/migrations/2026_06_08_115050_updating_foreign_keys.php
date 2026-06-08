9ab1a48d75e22886b0d340635d2fbf344d09c1bb
TheAbyssSage <sage.stockmans@pm.me>
2026-06-08 14:29:03 +0200

    wip: update classrooms foreign key migration

---
+++ database/migrations/2026_06_08_115050_updating_foreign_keys.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('classrooms', function (Blueprint $table) {
+            if (Schema::hasColumn('classrooms', 'teacher_id')) {
+                $table->dropConstrainedForeignId('teacher_id');
+            }
+
+            if (! Schema::hasColumn('classrooms', 'user_id')) {
+                $table->foreignId('user_id')->after('name')->constrained();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('classrooms', function (Blueprint $table) {
+            if (Schema::hasColumn('classrooms', 'user_id')) {
+                $table->dropConstrainedForeignId('user_id');
+            }
+
+            if (! Schema::hasColumn('classrooms', 'teacher_id')) {
+                $table->foreignId('teacher_id')->constrained();
+            }
+        });
+    }
+};
