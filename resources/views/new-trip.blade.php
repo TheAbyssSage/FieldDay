b7ebe693f7449aab356ec81f4875fc0a477a3f45
TheAbyssSage <sage.stockmans@pm.me>
2026-06-12 10:26:22 +0200

    Add new trip creation form for teachers

--- resources/views/new-trip.blade.php
+++ resources/views/new-trip.blade.php
@@ -1,6 +1,3 @@
 <x-layouts::app :title="__('New Trip')">
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl p-4">
-        <flux:heading size="xl">{{ __('New Trip') }}</flux:heading>
-        <flux:text>This is where the new trip form will go.</flux:text>
-    </div>
+    <livewire:teacher.trips.create />
 </x-layouts::app>
